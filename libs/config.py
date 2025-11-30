bcba3eb6badd0ce6f58e3beefa7053185b297157
vivatmax <161730424+vivatmax@users.noreply.github.com>
2025-11-30 13:23:03 +0300

    Update config.py

--- libs/config.py
+++ libs/config.py
@@ -6,5 +6,5 @@
 # Main Language : Python
 # Version RAT   : MythosR4T 1.0
 
-TOKEN = "5594316492:AAFk8FnR5Ra1Y1f5JyBKy_D7fL3VEv-FM_k" # GET TOKEN --> tme.to/BotFather
-ID = "1543290559" # GET ID --> tme.to/my_id_bot
+TOKEN = "" # GET TOKEN --> tme.to/BotFather
+ID = "" # GET ID --> tme.to/my_id_bot
